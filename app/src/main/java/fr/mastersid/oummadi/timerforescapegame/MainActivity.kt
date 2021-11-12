f8047ed4acab9e420440a26f0b77a3212c28f518
oummadi <n.oummadi.e@gmail.com>
2021-11-12 10:03:31 +0100

    configuration firebase

--- app/src/main/java/fr/mastersid/oummadi/timerforescapegame/MainActivity.kt
+++ app/src/main/java/fr/mastersid/oummadi/timerforescapegame/MainActivity.kt
@@ -1,8 +1,8 @@
 package fr.mastersid.oummadi.timerforescapegame
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
